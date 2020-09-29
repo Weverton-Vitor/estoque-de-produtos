29c2f3e44a20ea634d849618f034102e1317611a
weverton-vitor <wevertonvitor01@gmail.com>
2020-09-29 10:07:15 -0300

    Produto: produto_detail.html

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -5,5 +5,11 @@ def produto_list(request):
     template_name = 'produto_list.html'
     objects = Produto.objects.all()
     context = {'objects_list': objects}
+    return render(request, template_name, context)
 
+
+def produto_detail(request, pk):
+    template_name = 'produto_detail.html'
+    obj = Produto.objects.get(pk=pk)
+    context = {'object': obj}
     return render(request, template_name, context)
