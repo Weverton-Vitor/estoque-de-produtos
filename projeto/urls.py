bef419917a2303a35b015952e67973d7405d5595
weverton-vitor <wevertonvitor01@gmail.com>
2020-09-29 09:07:03 -0300

    Produto: produto_list.html

--- projeto/urls.py
+++ projeto/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('projeto.core.urls')),
+    path('produtos/', include('projeto.produto.urls')),
     path('admin/', admin.site.urls),
 ]
