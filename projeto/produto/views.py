030bdd53b328c7e043cb75edbe6f78cbd7bdf064
weverton-vitor <wevertonvitor01@gmail.com>
2020-09-30 17:38:46 -0300

    Produto: ProdutoForm

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from  .models import Produto
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from .models import Produto
+from .forms import ProdutoForm
+
 
 def produto_list(request):
     template_name = 'produto_list.html'
@@ -18,3 +22,12 @@ def produto_detail(request, pk):
 def produto_add(request):
     template_name = 'produto_form.html'
     return render(request, template_name)
+
+class ProdutoCreate(CreateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    form_class =  ProdutoForm
+
+    # Setando a url de sucesso para a página de detalhe do objeto
+    def get_success_url(self):
+        return self.object.get_absolute_url()
