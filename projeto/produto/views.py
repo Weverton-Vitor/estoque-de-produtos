ab01f7136492837f69cb039d69afed8c5fc834f6
weverton-vitor <wevertonvitor01@gmail.com>
2020-10-02 16:12:59 -0300

    Produto: ProdutoCreate

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -24,6 +24,7 @@ def produto_add(request):
     return render(request, template_name)
 
 class ProdutoCreate(CreateView):
+    # Create view
     model = Produto
     template_name = 'produto_form.html'
     form_class =  ProdutoForm
