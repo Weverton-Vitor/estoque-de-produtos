406d32e955670d1d7f822db181941cfe9c608271
weverton-vitor <wevertonvitor01@gmail.com>
2020-10-08 10:06:59 -0300

    Estoque Entrada: chamada ajax para atualizar o saldo de cada produto

--- projeto/produto/views.py
+++ projeto/produto/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView
@@ -37,3 +38,10 @@ class ProdutoUpdate(UpdateView):
     model = Produto
     template_name = 'produto_form.html'
     form_class = ProdutoForm
+
+
+def produto_json(request, pk):
+    """Retorna o produto, id e estoque"""
+    produto = Produto.objects.filter(pk=pk)
+    data = [item.to_dict_json() for item in produto]
+    return JsonResponse({'data': data})
