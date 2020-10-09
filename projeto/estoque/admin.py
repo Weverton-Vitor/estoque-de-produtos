5192391990504717fc4190a025a885952b78c0f6
weverton-vitor <wevertonvitor01@gmail.com>
2020-10-09 16:01:54 -0300

    Estoque Saida: proxy model

--- projeto/estoque/admin.py
+++ projeto/estoque/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Estoque, EstoqueItens
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 
 class EstoqueitensInline(admin.TabularInline):
     model = EstoqueItens
     extra = 0
 
-@admin.register(Estoque)
+@admin.register(EstoqueEntrada)
 class EstoqueAdmin(admin.ModelAdmin):
     inlines = (EstoqueitensInline,)
     list_display = ('__str__', 'nf', 'funcionario')
     search_fields = ('nr', )
     list_filter = ('funcionario', )
     date_hierarchy = 'created'
+
+@admin.register(EstoqueSaida)
+class EstoqueAdmin(admin.ModelAdmin):
+    inlines = (EstoqueitensInline,)
+    list_display = ('__str__', 'nf', 'funcionario')
+    search_fields = ('nr', )
+    list_filter = ('funcionario', )
+    date_hierarchy = 'created'
+
