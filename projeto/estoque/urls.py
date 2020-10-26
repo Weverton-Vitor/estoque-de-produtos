eafae783bbe72718b1270583ac1cd35cd409fdfe
weverton-vitor <wevertonvitor01@gmail.com>
2020-10-26 09:58:48 -0300

    Separando as urls de entrada e saida

--- projeto/estoque/urls.py
+++ projeto/estoque/urls.py
@@ -1,17 +1,21 @@
-from django.urls import path
+from django.urls import include, path
 from projeto.estoque import views as v
 from projeto.estoque.models import EstoqueEntrada
 
 app_name = 'estoque'
 
-urlpatterns = [
-    # Estoque de entrada
+entrada_patterns = [
     path('', v.EstoqueEntradaList.as_view(), name='estoque_entrada_list'),
-    path('<int:pk>/', v.EstoqueDetail.as_view(), name='estoque_detail'),
     path('add/', v.estoque_entrada_add, name='estoque_entrada_add'),
+]
 
-    # Estoque de Saida
-    path('saida', v.EstoqueSaidaList.as_view(), name='estoque_saida_list'),
-    path('saida/add/', v.estoque_saida_add, name='estoque_saida_add'),
+saida_patterns = [
+    path('', v.EstoqueSaidaList.as_view(), name='estoque_saida_list'),
+    path('add/', v.estoque_saida_add, name='estoque_saida_add'),
+]
 
+urlpatterns = [
+    path('entrada/', include(entrada_patterns)),
+    path('<int:pk>/', v.EstoqueDetail.as_view(), name='estoque_detail'),
+    path('saida/', include(saida_patterns))
 ]
